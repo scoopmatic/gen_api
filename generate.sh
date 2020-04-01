a82d8572e879100f278dab4aa0c63ac2cd7040a9
Samuel Rönnqvist <samuel.ronnqvist@gmail.com>
2020-04-01 12:29:48 +0300

    call new gen script

---
+++ generate.sh
@@ -0,0 +1,8 @@
+this=$1
+fname=$2
+
+source $this/../venv-stt/bin/activate
+
+#python $this/../OpenNMT-py/translate.py -model $this/../OpenNMT-py/hockey/saved_models/generation_model_nodalida.pt -src tmp_files/$fname.input -output tmp_files/$fname.output -replace_unk
+cd event2text-attnctrl
+bash generate_aug_live.sh ../tmp_files $fname.input $fname.output
