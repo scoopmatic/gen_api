e8be3e4e95c6668d8ec45f89e62eeb8b1ace3bff
Samuel Rönnqvist <samuel.ronnqvist@gmail.com>
2020-04-07 11:35:52 +0300

    gpu/cpu flag

--- generate_v2.sh
+++ generate_v2.sh
@@ -1,8 +1,10 @@
 this=$1
 fname=$2
 
+GPU=0 #0=Use first GPU; -1=Use CPU
+
 source $this/../venv-stt/bin/activate
 
 #python $this/../OpenNMT-py/translate.py -model $this/../OpenNMT-py/hockey/saved_models/generation_model_nodalida.pt -src tmp_files/$fname.input -output tmp_files/$fname.output -replace_unk
 cd event2text-attnctrl
-bash generate_aug_live.sh ../tmp_files $fname.input $fname.output
+bash generate_aug_live.sh ../tmp_files $fname.input $fname.output $GPU
